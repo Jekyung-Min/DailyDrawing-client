d38555fceb2bdb7adccc6021aa34cfaffac3868f
Jekyung-Min <cwm7485@naver.com>
2021-06-01 00:58:40 +0900

    feature_makeEnterFirst_#13: added enterFirst

---
+++ src/components/enter_first/enter_first.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from "./enter_first.module.css";
+
+const EnterFirst = (props) => {
+  return (
+    <div className={`${styles.home} ${styles.section}`}>
+      <div className={`${styles.container}`}>
+        <div className={`${styles.content}`}>
+          <div className={`${styles.social}`}></div>
+          <img
+            className={`${styles.img}`}
+            src="/images/western1.jpeg"
+            alt="western1"
+          />
+          <div className={`${styles.data}`}>
+            <h1 className={`${styles.title}`}>DailyDrawing</h1>
+            <h3 className={`${styles.subtitle}`}>
+              예술가, 화가 되고싶으신가요?
+            </h3>
+            <p className={`${styles.description}`}>
+              DailyDrawing과 함께 꿈을 이루어 보세요.
+            </p>
+            <a href="/" className={`${styles.button} ${styles.button_flex}`}>
+              체험하기
+            </a>
+          </div>
+        </div>
+
+        <div className={`${styles.scroll}`}>
+          <a
+            href="/"
+            className={`${styles.scroll_button} ${styles.button_flex}`}
+          >
+            <i class={`uil uil-mouse-alt ${styles.scroll_mouse}`}></i>
+            <span className={`${styles.scroll_name}`}>더보기</span>
+            <i className={`uil uil-arrow-down ${styles.scroll_arrow}`}></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EnterFirst;
