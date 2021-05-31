132c1c0eeecd9a168eb582cecd43e592ffa6e2a3
Jekyung-Min <cwm7485@naver.com>
2021-06-01 01:31:25 +0900

    feature_makeCommunityPage_#18: added communityPage

---
+++ src/pages/communityPage/communityPage.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import IconButton from "@material-ui/core/IconButton";
+import styles from "./communityPage.module.css";
+import { Link } from "react-router-dom";
+import CommunityHeader from "../../components/community_header/community_header";
+import CommunityMain from "../../components/community_main/community_main";
+import Navbar from "../../components/navbar/navbar";
+import unsplash from "../../api/unsplash";
+import Footer from "../../components/footer/footer";
+
+const CommunityPage = () => {
+  const [pins, setNewPins] = useState([]);
+
+  const getImages = (term) => {
+    return unsplash.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+    });
+  };
+  const onSearchSubmit = (term) => {
+    getImages(term).then((res) => {
+      const results = res.data.results;
+      const newPins = [...results, ...pins];
+      setNewPins(newPins);
+    });
+  };
+
+  const getNewPins = () => {
+    const promises = [];
+    let pinData = [];
+
+    const pins = [
+      "acrylic",
+      "watercolor",
+      "oriental painting",
+      "painting",
+      "drawing",
+    ];
+
+    // 여러가지 섞어서
+    pins.forEach((pinTerm) => {
+      promises.push(
+        getImages(pinTerm).then((res) => {
+          const results = res.data.results;
+          pinData = pinData.concat(results);
+          pinData.sort(function (a, b) {
+            return 0.5 - Math.random();
+          });
+        })
+      );
+    });
+
+    Promise.all(promises).then(() => {
+      setNewPins(pinData);
+    });
+
+    //하나만 나오게 하기
+
+    // getImages(pins[Math.floor(Math.random() * 5)]).then((res) => {
+    //   const results = res.data.results;
+    //   setNewPins(results);
+    // });
+  };
+  useEffect(() => {
+    getNewPins();
+  }, []);
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <CommunityHeader onSubmit={onSearchSubmit} />
+      <CommunityMain pins={pins} />
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default CommunityPage;
