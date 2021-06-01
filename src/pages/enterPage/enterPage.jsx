31d610502545b2cb8fb165dbf4315feffdf13b62
Jekyung-Min <cwm7485@naver.com>
2021-06-02 02:58:42 +0900

    feature_makeEnterFourth_#28: added enterPage

--- src/pages/enterPage/enterPage.jsx
+++ src/pages/enterPage/enterPage.jsx
@@ -3,12 +3,18 @@ import Footer from "../../components/footer/footer";
 import Navbar from "../../components/navbar/navbar";
 import EnterFirst from "../../components/enter_first/enter_first";
 import EnterSecond from "../../components/enter_second/enter_second";
+import EnterThird from "../../components/enter_third/enter_third";
+import EnterFourth from "../../components/enter_fourth/enter_fourth";
+import EnterFifth from "../../components/enter_fifth/enter_fifth";
 const EnterPage = (props) => {
   return (
     <>
       <Navbar></Navbar>
       <EnterFirst></EnterFirst>
+      <EnterThird></EnterThird>
       <EnterSecond></EnterSecond>
+      <EnterFourth></EnterFourth>
+      <EnterFifth></EnterFifth>
       <Footer></Footer>
     </>
   );
