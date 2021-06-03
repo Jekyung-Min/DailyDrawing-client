8be0ddd78fc2ba7e5aa8a2be2ee801758d5c9b32
Jekyung-Min <cwm7485@naver.com>
2021-06-03 20:07:20 +0900

    feature_makeEnterSixth_#33: modified EnterPage

--- src/pages/enterPage/enterPage.jsx
+++ src/pages/enterPage/enterPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "../../components/footer/footer";
+import FooterBelow from "../../components/footer_below/footer_below";
 import Navbar from "../../components/navbar/navbar";
 import EnterFirst from "../../components/enter_first/enter_first";
 import EnterSecond from "../../components/enter_second/enter_second";
 import EnterThird from "../../components/enter_third/enter_third";
 import EnterFourth from "../../components/enter_fourth/enter_fourth";
 import EnterFifth from "../../components/enter_fifth/enter_fifth";
+import EnterSixth from "../../components/enter_sixth/enter_sixth";
 const EnterPage = (props) => {
   return (
     <>
@@ -15,6 +17,8 @@ const EnterPage = (props) => {
       <EnterSecond></EnterSecond>
       <EnterFourth></EnterFourth>
       <EnterFifth></EnterFifth>
+      <EnterSixth></EnterSixth>
+      <FooterBelow></FooterBelow>
       <Footer></Footer>
     </>
   );
