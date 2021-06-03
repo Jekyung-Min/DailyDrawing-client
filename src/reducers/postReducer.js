461febe8a36fe8a4a3983e613ed9d2308cd03364
민제현 <alswpgus0@gmail.com>
2021-06-04 00:00:20 +0900

    feature_makeReducers_#37:Added postReducer

---
+++ src/reducers/postReducer.js
@@ -0,0 +1,11 @@
+import {} from "../actions";
+import { initialState } from "./initialState";
+
+const postReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default postReducer;
