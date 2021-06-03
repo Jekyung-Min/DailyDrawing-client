2c10ed237c53ef2c2e23fec7b441928eb59ab943
민제현 <alswpgus0@gmail.com>
2021-06-04 00:00:46 +0900

    feature_makeReducers_#37:Added commentReducer

---
+++ src/reducers/commentReducer.js
@@ -0,0 +1,11 @@
+import {} from "../actions";
+import { initialState } from "./initialState";
+
+const commentReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default commentReducer;
