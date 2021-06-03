0e62d71f59f3a1b9ac11f4498d8acd39d34bd56e
민제현 <alswpgus0@gmail.com>
2021-06-04 01:43:18 +0900

    feature_makeAction_#44:Modified action index.js to add action type

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,21 @@
 import axios from "axios";
 
 // action types
-
-// middleware
+// sign part
+export const SIGN_IN = "SIGN_IN";
+export const SIGN_OUT = "SIGN_OUT";
+// user part
+export const GET_USER_INFO = "GET_USER_INFO";
+// post part
+export const GET_POST_INFO = "GET_POST_INFO";
+export const GETALL_POST_INFO = "GETALL_POST_INFO";
+// comment part
+export const GET_POST_COMMENTS = "GET_POST_COMMENTS";
+// tag part
+export const GET_POST_TAGS = "GET_POST_TAGS";
+// like part
+export const GET_POSTLIKE_COUNT = "GET_POSTLIKE_COUNT";
+export const GET_MY_LIKES = "GET_MY_LIKES";
 
 // actions creator functions
 
