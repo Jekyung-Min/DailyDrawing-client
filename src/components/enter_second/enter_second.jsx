422a228b7978f0664c69e0aecc6ef7b42ab67288
Jekyung-Min <cwm7485@naver.com>
2021-06-04 09:33:11 +0900

    feature_makeScrollAction_#47: added Fade effect in second

--- src/components/enter_second/enter_second.jsx
+++ src/components/enter_second/enter_second.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./enter_second.module.css";
+import Fade from "react-reveal/Fade";
 
 const EnterSecond = ({ aboutRef }) => {
   return (
@@ -10,56 +11,62 @@ const EnterSecond = ({ aboutRef }) => {
       </span>
 
       <div className={`${styles.about_container} ${styles.container}`}>
-        <img
-          src="/images/dailydrawing1.jpg"
-          alt="dailydrawing1"
-          className={`${styles.about_img}`}
-        ></img>
+        <Fade left duration={1800} distance={"30px"}>
+          <img
+            src="/images/dailydrawing1.jpg"
+            alt="dailydrawing1"
+            className={`${styles.about_img}`}
+          ></img>
+        </Fade>
+        <Fade right duration={1800} distance={"30px"}>
+          <div className={`${styles.about_data}`}>
+            <p className={`${styles.about_description}`}>
+              약 7년간 220,000명이 참여해주셨습니다.
+              <br />
+              <br />
+              지금까지 누적 2,000,000개의 완성작을 남겨주셨으며
+              <br />
+              <br />
+              DailyDrawing의 파트너사는 홍익대학교를 포함하여
+              <br />
+              <br />
+              500군데를 돌파하였습니다.
+            </p>
 
-        <div className={`${styles.about_data}`}>
-          <p className={`${styles.about_description}`}>
-            약 7년간 220,000명이 참여해주셨습니다.
-            <br />
-            <br />
-            지금까지 누적 2,000,000개의 완성작을 남겨주셨으며
-            <br />
-            <br />
-            DailyDrawing의 파트너사는 홍익대학교를 포함하여
-            <br />
-            <br />
-            500군데를 돌파하였습니다.
-          </p>
+            <div className={`${styles.about_info}`}>
+              <div>
+                <span className={`${styles.about_info_title}`}>07+</span>
+                <span className={`${styles.about_info_name}`}>
+                  운영
+                  <br /> 총기간
+                </span>
+              </div>
 
-          <div className={`${styles.about_info}`}>
-            <div>
-              <span className={`${styles.about_info_title}`}>07+</span>
-              <span className={`${styles.about_info_name}`}>
-                운영
-                <br /> 총기간
-              </span>
+              <div>
+                <span className={`${styles.about_info_title}`}>2,000,000+</span>
+                <span className={`${styles.about_info_name}`}>
+                  누적 <br /> 완성작
+                </span>
+              </div>
+
+              <div>
+                <span className={`${styles.about_info_title}`}>500+</span>
+                <span className={`${styles.about_info_name}`}>
+                  협력 <br /> 파트너사
+                </span>
+              </div>
             </div>
 
-            <div>
-              <span className={`${styles.about_info_title}`}>2,000,000+</span>
-              <span className={`${styles.about_info_name}`}>
-                누적 <br /> 완성작
-              </span>
-            </div>
-
-            <div>
-              <span className={`${styles.about_info_title}`}>500+</span>
-              <span className={`${styles.about_info_name}`}>
-                협력 <br /> 파트너사
-              </span>
+            <div className={`${styles.about_buttons}`}>
+              <a
+                href="/community"
+                className={`${styles.button} ${styles.button_flex}`}
+              >
+                체험하기
+              </a>
             </div>
           </div>
-
-          <div className={`${styles.about_buttons}`}>
-            <a href="/" className={`${styles.button} ${styles.button_flex}`}>
-              체험하기
-            </a>
-          </div>
-        </div>
+        </Fade>
       </div>
     </div>
   );
