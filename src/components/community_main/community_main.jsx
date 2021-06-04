61c0bbfbe5f0c1f9fcba5f75f37fa7b3069312e6
Jekyung-Min <cwm7485@naver.com>
2021-06-04 09:35:11 +0900

    feature_makeScrollAction_#47: added Fade Effect in CommunityPin

--- src/components/community_main/community_main.jsx
+++ src/components/community_main/community_main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./community_main.module.css";
 import CommunityPin from "../community_pin/community_pin";
+import Fade from "react-reveal/Fade";
 
 const CommunityMain = ({ pins }) => {
   return (
@@ -8,7 +9,13 @@ const CommunityMain = ({ pins }) => {
       <div className={styles.container}>
         {pins.map((pin, idx) => {
           const { urls } = pin;
-          return <CommunityPin urls={urls} key={idx} />;
+          return (
+            <>
+              <Fade top duration={1800} distance={"30px"}>
+                <CommunityPin urls={urls} key={idx} />
+              </Fade>
+            </>
+          );
         })}
       </div>
     </div>
