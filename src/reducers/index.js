714fc1b3c8094f9a4e4684d7c1fd05b8039dd98d
민제현 <alswpgus0@gmail.com>
2021-06-04 12:11:59 +0900

    feature_connectActionToReducer_#51:Modified searchReducer to connect action type

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,13 +4,14 @@ import postReducer from "./postReducer";
 import commentReducer from "./commentReducer";
 import tagReducer from "./tagReducer";
 import likeReducer from "./likeReducer";
-
+import searchReducer from "./searchReducer";
 const rootReducer = combineReducers({
   userReducer,
   postReducer,
   commentReducer,
   tagReducer,
   likeReducer,
+  searchReducer,
 });
 
 export default rootReducer;
