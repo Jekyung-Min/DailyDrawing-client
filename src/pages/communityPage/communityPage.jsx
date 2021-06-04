01fe3f5cda45d76eb58e37346dca8987d768ced9
민제현 <alswpgus0@gmail.com>
2021-06-04 13:30:18 +0900

    feature_reqestPostDataFromServer_#55:Modified communityPage component to delete unsplash code and to add postDatas from server

--- src/pages/communityPage/communityPage.jsx
+++ src/pages/communityPage/communityPage.jsx
@@ -1,77 +1,38 @@
 import React, { useEffect, useRef, useState } from "react";
-import IconButton from "@material-ui/core/IconButton";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchData, getAllPostInfo } from "../../actions";
 import styles from "./communityPage.module.css";
 import { Link } from "react-router-dom";
 import CommunityHeader from "../../components/community_header/community_header";
 import CommunityMain from "../../components/community_main/community_main";
 import Navbar from "../../components/navbar/navbar";
-import unsplash from "../../api/unsplash";
 import Footer from "../../components/footer/footer";
 import { CommunityModal } from "../../components/community_modal/community_modal";
 
+const URL = process.env.REACT_APP_SERVER_URL;
 const CommunityPage = () => {
+  const postsState = useSelector(state => state.postReducer);
+  const dispatch = useDispatch();
   const [showModal, setShowModal] = useState(false);
-  const [pins, setNewPins] = useState([]);
 
   useEffect(() => {
     window.scroll({
       top: 0,
       behavior: "smooth",
     });
-    getNewPins();
+
+    dispatch(fetchData(`${URL}/drawing/getall`, {}, getAllPostInfo));
   }, []);
 
   const openModal = () => {
-    setShowModal((prev) => !prev);
+    setShowModal(prev => !prev);
   };
 
-  const getImages = (term) => {
-    return unsplash.get("https://api.unsplash.com/search/photos", {
-      params: { query: term },
-    });
-  };
-  const onSearchSubmit = (term) => {
-    getImages(term).then((res) => {
-      const results = res.data.results;
-      const newPins = [...results, ...pins];
-      setNewPins(newPins);
-    });
-  };
-
-  const getNewPins = () => {
-    const promises = [];
-    let pinData = [];
-
-    const pins = [
-      "acrylic",
-      "watercolor",
-      "oriental painting",
-      "painting",
-      "drawing",
-    ];
-
-    // 여러가지 섞어서
-    pins.forEach((pinTerm) => {
-      promises.push(
-        getImages(pinTerm).then((res) => {
-          const results = res.data.results;
-          pinData = pinData.concat(results);
-          pinData.sort(function (a, b) {
-            return 0.5 - Math.random();
-          });
-        })
-      );
-    });
-
-    Promise.all(promises).then(() => {
-      setNewPins(pinData);
-    });
-
-    //하나만 나오게 하기
-
-    // getImages(pins[Math.floor(Math.random() * 5)]).then((res) => {
+  const onSearchSubmit = term => {
+    // getImages(term).then(res => {
     //   const results = res.data.results;
-    //   setNewPins(results);
+    //   const newPins = [...results, ...pins];
+    //   setNewPins(newPins);
     // });
   };
 
@@ -83,7 +44,7 @@ const CommunityPage = () => {
       ></CommunityModal>
       <Navbar link="listPage"></Navbar>
       <CommunityHeader onSubmit={onSearchSubmit} />
-      <CommunityMain openModal={openModal} pins={pins} />
+      <CommunityMain openModal={openModal} />
       <Footer></Footer>
     </>
   );
