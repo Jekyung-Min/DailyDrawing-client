86cb3a969d148b20bb57e0824bdcea86566ad951
Jekyung-Min <cwm7485@naver.com>
2021-06-04 09:40:49 +0900

    feature_makeScrollAction_#47: added Effect in sixth

--- src/components/enter_sixth/enter_sixth.jsx
+++ src/components/enter_sixth/enter_sixth.jsx
@@ -8,6 +8,7 @@ import SwiperCore, {
 } from "swiper";
 import "swiper/swiper-bundle.css";
 import styles from "./enter_sixth.module.css";
+import Fade from "react-reveal/Fade";
 
 SwiperCore.use([Navigation, Pagination, Autoplay, Mousewheel]);
 
@@ -21,212 +22,213 @@ const EnterSixth = ({ portfolioRef }) => {
       <span className={`${styles.section_subtitle}`}>
         DailyDrawing 수료생 대표작품
       </span>
-
-      <Swiper
-        className={`${styles.portfolio_container} ${styles.container}`}
-        tag="section"
-        cssMode
-        loop={true}
-        grabCursor={true}
-        slidesPerView={2}
-        pagination={{
-          clickable: true,
-          // dynamicBullets: true
-        }}
-      >
-        <div className={`${styles.portfolio_container} ${styles.container}`}>
-          <div className={`swiper-wrapper`}>
-            <SwiperSlide
-              className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
-            >
-              <div
+      <Fade left duration={1800} distance={"30px"}>
+        <Swiper
+          className={`${styles.portfolio_container} ${styles.container}`}
+          tag="section"
+          cssMode
+          loop={true}
+          grabCursor={true}
+          slidesPerView={2}
+          pagination={{
+            clickable: true,
+            // dynamicBullets: true
+          }}
+        >
+          <div className={`${styles.portfolio_container} ${styles.container}`}>
+            <div className={`swiper-wrapper`}>
+              <SwiperSlide
                 className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
               >
-                <img
-                  src="images/image06.jpeg"
-                  alt="image06"
-                  className={`${styles.portfolio_img}`}
-                />
+                <div
+                  className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
+                >
+                  <img
+                    src="images/image06.jpeg"
+                    alt="image06"
+                    className={`${styles.portfolio_img}`}
+                  />
 
-                <div className={`${styles.portfolio_data}`}>
-                  <h3 className={`${styles.portfolio_title}`}>
-                    민제경 - Woman with Rainbow
-                  </h3>
-                  <p className={`${styles.portfolio_description}`}>
-                    리움미술관 주관대회 대상작
-                  </p>
+                  <div className={`${styles.portfolio_data}`}>
+                    <h3 className={`${styles.portfolio_title}`}>
+                      민제경 - Woman with Rainbow
+                    </h3>
+                    <p className={`${styles.portfolio_description}`}>
+                      리움미술관 주관대회 대상작
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </SwiperSlide>
+              </SwiperSlide>
 
-            <SwiperSlide
-              className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
-            >
-              <div
+              <SwiperSlide
                 className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
               >
-                <img
-                  src="images/image48.jpeg"
-                  alt="image48"
-                  className={`${styles.portfolio_img}`}
-                  style={{ width: "300px" }}
-                />
+                <div
+                  className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
+                >
+                  <img
+                    src="images/image48.jpeg"
+                    alt="image48"
+                    className={`${styles.portfolio_img}`}
+                    style={{ width: "300px" }}
+                  />
 
-                <div className={`${styles.portfolio_data}`}>
-                  <h3 className={`${styles.portfolio_title}`}>
-                    민제현 - Black Ocean
-                  </h3>
-                  <p className={`${styles.portfolio_description}`}>
-                    홍익대학교 대학원 공모전 작품상
-                  </p>
+                  <div className={`${styles.portfolio_data}`}>
+                    <h3 className={`${styles.portfolio_title}`}>
+                      민제현 - Black Ocean
+                    </h3>
+                    <p className={`${styles.portfolio_description}`}>
+                      홍익대학교 대학원 공모전 작품상
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide
-              className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
-            >
-              <div
+              </SwiperSlide>
+              <SwiperSlide
                 className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
               >
-                <img
-                  src="images/image55.jpeg"
-                  alt="image55"
-                  className={`${styles.portfolio_img}`}
-                />
+                <div
+                  className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
+                >
+                  <img
+                    src="images/image55.jpeg"
+                    alt="image55"
+                    className={`${styles.portfolio_img}`}
+                  />
 
-                <div className={`${styles.portfolio_data}`}>
-                  <h3 className={`${styles.portfolio_title}`}>
-                    함민우 - Lava from Heart
-                  </h3>
-                  <p className={`${styles.portfolio_description}`}>
-                    서울시립미술관 주관대회 특상
-                  </p>
+                  <div className={`${styles.portfolio_data}`}>
+                    <h3 className={`${styles.portfolio_title}`}>
+                      함민우 - Lava from Heart
+                    </h3>
+                    <p className={`${styles.portfolio_description}`}>
+                      서울시립미술관 주관대회 특상
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide
-              className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
-            >
-              <div
+              </SwiperSlide>
+              <SwiperSlide
                 className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
               >
-                <img
-                  src="images/image16.jpeg"
-                  alt="image16"
-                  className={`${styles.portfolio_img}`}
-                />
+                <div
+                  className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
+                >
+                  <img
+                    src="images/image16.jpeg"
+                    alt="image16"
+                    className={`${styles.portfolio_img}`}
+                  />
 
-                <div className={`${styles.portfolio_data}`}>
-                  <h3 className={`${styles.portfolio_title}`}>
-                    고상아 - Marilyn Monroe
-                  </h3>
-                  <p className={`${styles.portfolio_description}`}>
-                    석파정미술관 주관대회 혁신상
-                  </p>
+                  <div className={`${styles.portfolio_data}`}>
+                    <h3 className={`${styles.portfolio_title}`}>
+                      고상아 - Marilyn Monroe
+                    </h3>
+                    <p className={`${styles.portfolio_description}`}>
+                      석파정미술관 주관대회 혁신상
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </SwiperSlide>
+              </SwiperSlide>
 
-            <SwiperSlide
-              className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
-            >
-              <div
+              <SwiperSlide
                 className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
               >
-                <img
-                  src="images/image52.jpeg"
-                  alt="image52"
-                  className={`${styles.portfolio_img}`}
-                />
+                <div
+                  className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
+                >
+                  <img
+                    src="images/image52.jpeg"
+                    alt="image52"
+                    className={`${styles.portfolio_img}`}
+                  />
 
-                <div className={`${styles.portfolio_data}`}>
-                  <h3 className={`${styles.portfolio_title}`}>
-                    오윤희 - Beauty Insight
-                  </h3>
-                  <p className={`${styles.portfolio_description}`}>
-                    국민대학교 공모전 크로키상
-                  </p>
+                  <div className={`${styles.portfolio_data}`}>
+                    <h3 className={`${styles.portfolio_title}`}>
+                      오윤희 - Beauty Insight
+                    </h3>
+                    <p className={`${styles.portfolio_description}`}>
+                      국민대학교 공모전 크로키상
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide
-              className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
-            >
-              <div
+              </SwiperSlide>
+              <SwiperSlide
                 className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
               >
-                <img
-                  src="images/image57.jpeg"
-                  alt="image57"
-                  className={`${styles.portfolio_img}`}
-                />
+                <div
+                  className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
+                >
+                  <img
+                    src="images/image57.jpeg"
+                    alt="image57"
+                    className={`${styles.portfolio_img}`}
+                  />
 
-                <div className={`${styles.portfolio_data}`}>
-                  <h3 className={`${styles.portfolio_title}`}>
-                    심수련 - Arabian Woman
-                  </h3>
-                  <p className={`${styles.portfolio_description}`}>
-                    한가람미술관 주관대회 콜라주상
-                  </p>
+                  <div className={`${styles.portfolio_data}`}>
+                    <h3 className={`${styles.portfolio_title}`}>
+                      심수련 - Arabian Woman
+                    </h3>
+                    <p className={`${styles.portfolio_description}`}>
+                      한가람미술관 주관대회 콜라주상
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide
-              className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
-            >
-              <div
+              </SwiperSlide>
+              <SwiperSlide
                 className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
               >
-                <img
-                  src="images/image22.jpeg"
-                  alt="image22"
-                  className={`${styles.portfolio_img}`}
-                />
+                <div
+                  className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
+                >
+                  <img
+                    src="images/image22.jpeg"
+                    alt="image22"
+                    className={`${styles.portfolio_img}`}
+                  />
 
-                <div className={`${styles.portfolio_data}`}>
-                  <h3 className={`${styles.portfolio_title}`}>
-                    하윤철 - Navy Love
-                  </h3>
-                  <p className={`${styles.portfolio_description}`}>
-                    디뮤지엄 공모전 은상
-                  </p>
+                  <div className={`${styles.portfolio_data}`}>
+                    <h3 className={`${styles.portfolio_title}`}>
+                      하윤철 - Navy Love
+                    </h3>
+                    <p className={`${styles.portfolio_description}`}>
+                      디뮤지엄 공모전 은상
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide
-              className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
-            >
-              <div
+              </SwiperSlide>
+              <SwiperSlide
                 className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
               >
-                <img
-                  src="images/image53.jpeg"
-                  alt="image53"
-                  className={`${styles.portfolio_img}`}
-                />
+                <div
+                  className={`${styles.portfolio_content} ${styles.grid} swiper-slide`}
+                >
+                  <img
+                    src="images/image53.jpeg"
+                    alt="image53"
+                    className={`${styles.portfolio_img}`}
+                  />
 
-                <div className={`${styles.portfolio_data}`}>
-                  <h3 className={`${styles.portfolio_title}`}>
-                    백현준 - Eyes of Truth
-                  </h3>
-                  <p className={`${styles.portfolio_description}`}>
-                    국립현대미술관 주관대회 혁신상
-                  </p>
+                  <div className={`${styles.portfolio_data}`}>
+                    <h3 className={`${styles.portfolio_title}`}>
+                      백현준 - Eyes of Truth
+                    </h3>
+                    <p className={`${styles.portfolio_description}`}>
+                      국립현대미술관 주관대회 혁신상
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </SwiperSlide>
+              </SwiperSlide>
+            </div>
+
+            <div className={`swiper-button-next`}>
+              <i className={`uil uil-angle-right-b swiper-portfolio-icon`}></i>
+            </div>
+            <div className={`swiper-button-prev`}>
+              <i className={`uil uil-angle-left-b swiper-portfolio-icon`}></i>
+            </div>
+
+            <div className={`swiper-pagination`}></div>
           </div>
-
-          <div className={`swiper-button-next`}>
-            <i className={`uil uil-angle-right-b swiper-portfolio-icon`}></i>
-          </div>
-          <div className={`swiper-button-prev`}>
-            <i className={`uil uil-angle-left-b swiper-portfolio-icon`}></i>
-          </div>
-
-          <div className={`swiper-pagination`}></div>
-        </div>
-      </Swiper>
+        </Swiper>
+      </Fade>
     </section>
   );
 };
